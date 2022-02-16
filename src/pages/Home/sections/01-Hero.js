2249aea6b5d837e26cc0e5b35651a62ec1d09100
Dallen Hoyal <dallen.hoyal@gmail.com>
2022-02-15 20:25:46 -0700

    mobile hero

--- src/pages/Home/sections/01-Hero.js
+++ src/pages/Home/sections/01-Hero.js
@@ -6,6 +6,7 @@ import AppearAnimation from 'components/AppearAnimation'
 
 import colors from 'styles/colors'
 import text from 'styles/text'
+import media from 'styles/media'
 
 import CloudPNG from 'images/cloud.png'
 
@@ -40,6 +41,7 @@ const Hero = () => {
     }, {
       duration: 1,
       ease: 'circ.out',
+      opacity: 1,
       y: '0%'
     }, 1.5)
 
@@ -76,7 +78,12 @@ const Wrapper = styled.section`
   position: relative;
   z-index: 3;
   width: 100%;
+
   height: 56.25vw;
+
+  ${media.mobile} {
+    height: 244vw;
+  }
 `
 
 const Title1 = styled.h1`
@@ -88,6 +95,14 @@ const Title1 = styled.h1`
   width: 46.181vw;
   top: 10.417vw;
   left: 11.25vw;
+
+  ${media.mobile} {
+    ${text.mobile.h1}
+    width: 74.133vw;
+    top: 82vw;
+    left: 50%;
+    transform: translateX(-50%);
+  }
 `
 
 const Title2 = styled.h2`
@@ -99,6 +114,14 @@ const Title2 = styled.h2`
   width: 31.944vw;
   top: 17.014vw;
   left: 17.639vw;
+
+  ${media.mobile} {
+    ${text.mobile.h5}
+    width: 46.133vw;
+    top: 90.933vw;
+    left: 50%;
+    transform: translateX(-50%);
+  }
 `
 
 const Title3 = styled.h2`
@@ -110,36 +133,68 @@ const Title3 = styled.h2`
   width: 52.847vw;
   top: 33.333vw;
   left: 32.014vw;
+
+  ${media.mobile} {
+    ${text.mobile.h1}
+    width: 85.067vw;
+    left: 50%;
+    transform: translateX(-50%);
+    top: 120vw;
+  }
 `
 
 const Cloud1 = styled.img`
   position: absolute;
   z-index: 1;
+  opacity: 0;
 
   right: -0.625vw;
   top: 54.444vw;
   width: 58.264vw;
   height: 33.264vw;
+
+  ${media.mobile} {
+    right: 25.867vw;
+    top: 185.6vw;
+    width: 112vw;
+    height: 63.733vw;
+  }
 `
 
 const Cloud2 = styled.img`
   position: absolute;
   transform: rotate(180deg);
   z-index: 1;
+  opacity: 0;
 
   top: 37.778vw;
   left: -19.167vw;
   width: 88.75vw;
   height: 50.764vw;
+  
+  ${media.mobile} {
+    top: 168.533vw;
+    left: 55.2vw;
+    width: 71.467vw;
+    height: 40.8vw;
+  }
 `
 
 const Cloud3 = styled.img`
   position: absolute;
   transform: rotate(180deg);
   z-index: 1;
+  opacity: 0;
 
   right: -5.833vw;
   top: 45.556vw;
   width: 37.292vw;
   height: 21.319vw;
+
+  ${media.mobile} {
+    right: -15.733vw;
+    top: 153.6vw;
+    width: 170.4vw;
+    height: 97.333vw;
+  }
 `
--- src/styles/text.js
+++ src/styles/text.js
@@ -139,6 +139,76 @@ const text = {
       line-height: 90%;
     `
   },
+  mobile: {
+    h1: `
+      font-family: Love;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 21.333vw;
+      line-height: 90%;      
+      letter-spacing: -0.03em;
+    `,
+    h2: `
+      font-family: Love;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 16vw;
+      line-height: 90%;
+      letter-spacing: -0.05em;
+    `,
+    h3: `
+      font-family: Love;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 80vw;
+      line-height: 90%;
+      letter-spacing: -0.03em;
+    `,
+    h4: `
+      font-family: Love;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 53.333vw;
+      line-height: 90%;
+      letter-spacing: -0.03em;
+    `,
+    h5: `
+      font-family: Love;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 40vw;
+      line-height: 90%;      
+      letter-spacing: -0.03em;
+    `,
+    h6: `
+      font-family: Love;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 26.4vw;
+      line-height: 95%;
+    `,
+    h7: `
+      font-family: SangBleu Sunrise;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 10.667vw;
+      line-height: 130%;
+    `,
+    body: `
+      font-family: SangBleu Sunrise, sans-serif;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 5.333vw;
+      line-height: 150%;
+    `,
+    nav: `
+      font-family: SangBleu Sunrise, sans-serif;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 4vw;
+      line-height: 90%;
+    `
+  },
 }
 
 export default text
