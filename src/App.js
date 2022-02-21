999b508ae9ce8e869fba9e74620077e4ae40e4be
Dallen Hoyal <dallen.hoyal@gmail.com>
2022-02-21 12:19:55 -0700

    providers.js

--- src/App.js
+++ src/App.js
@@ -4,24 +4,27 @@ import { Routes, Route } from 'react-router-dom'
 
 import colors from 'styles/colors'
 
-import Scroll from 'components/Scroll'
-import Header from 'components/Header'
+import Providers  from 'components/Providers'
+import Scroll     from 'components/Scroll'
+import Header     from 'components/Header'
 
 import Home   from 'pages/Home'
 import Travel from 'pages/Travel'
 
 const App = () => {
   return (
-    <Wrapper className='smooth-scroll'>
-      <Scroll/>
-      <Header/>
+    <Providers>
+      <Wrapper className='smooth-scroll'>
+        <Scroll/>
+        <Header/>
 
-      <Routes>
-        <Route path="/"       element={<Home/>}   />
-        <Route path="/travel" element={<Travel/>} />
-      </Routes>
+        <Routes>
+          <Route path="/"       element={<Home/>}   />
+          <Route path="/travel" element={<Travel/>} />
+        </Routes>
 
-    </Wrapper>
+      </Wrapper>
+    </Providers>
   );
 }
 
