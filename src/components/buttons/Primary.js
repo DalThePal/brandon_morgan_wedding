52c8a44dd6f4bf8ddffe982d661164555a75ecc4
Dallen Hoyal <dallen.hoyal@gmail.com>
2022-02-26 16:26:07 -0700

    magnetic buttons

--- src/components/buttons/Primary.js
+++ src/components/buttons/Primary.js
@@ -1,5 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useContext, useCallback } from 'react'
 import styled from 'styled-components'
+import gsap from 'gsap'
+
+import { ScreenContext } from 'components/Providers'
 
 import colors from 'styles/colors'
 import text   from 'styles/text'
@@ -9,13 +12,49 @@ import Arrows from 'components/Arrows'
 
 const Primary = ({ children, onClick }) => {
 
+  const screen = useContext(ScreenContext)
+
   const [trigger, setTrigger] = useState(false)
 
+  const buttonRef = useRef(null)
+
+  const mouseMove = useCallback((e) => {
+    const clientRect = buttonRef.current?.getBoundingClientRect()
+    const x = ((e.clientX) - clientRect.x - (clientRect.width / 2))
+    const y = e.clientY - (clientRect.y) - (clientRect.height / 2)
+
+    gsap.to(buttonRef.current, {
+      duration: 1,
+      x: x,
+      y: y,
+    })
+  }, [])
+
+  const onEnter = () => {
+    if (!screen.mobile && !screen.tablet) {
+      window.addEventListener("mousemove", mouseMove)
+    }
+  }
+
+  const onLeave = () => {
+    if (!screen.mobile && !screen.tablet) {
+      window.removeEventListener("mousemove", mouseMove)
+
+      gsap.to(buttonRef.current, {
+        duration: 1,
+        x: 0,
+        y: 0,
+      })
+    }
+  }
+
   return (
-    <Wrapper onMouseEnter={() => setTrigger(true)} onMouseLeave={() => setTrigger(false)} onClick={onClick}>
-      <Arrows left trigger={trigger} color={colors.mauve600}/>
-      {children}
-      <Arrows trigger={trigger} color={colors.mauve600}/>
+    <Wrapper onMouseEnter={onEnter} onMouseLeave={onLeave} onClick={onClick}>
+      <Button ref={buttonRef} onMouseEnter={() => setTrigger(true)} onMouseLeave={() => setTrigger(false)}>
+        <Arrows left trigger={trigger} color={colors.mauve600}/>
+        {children}
+        <Arrows trigger={trigger} color={colors.mauve600}/>
+      </Button>
     </Wrapper>
   )
 }
@@ -23,6 +62,10 @@ const Primary = ({ children, onClick }) => {
 export default Primary
 
 const Wrapper = styled.div`
+
+`
+
+const Button = styled.div`
   ${text.desktop.nav}
   background-color: ${colors.pink400};
   color: ${colors.mauve600};
