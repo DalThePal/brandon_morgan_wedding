5c18cded413c93ba64c126d327a7635815b3c440
Dallen Hoyal <dallen.hoyal@gmail.com>
2022-07-31 17:56:06 -0600

    rsvp

--- src/pages/RSVP/index.js
+++ src/pages/RSVP/index.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import Footer from 'components/Footer'
+import Scroll from 'components/Scroll'
 
 import Hero from './sections/01-Hero'
 
 const RSVP = () => {
   return (
-    <>
+    <Scroll>
       <Hero/>
       <Footer
         diamond
         leftText={"We're getting married. You're getting a hangover."}
       />
-    </>
+    </Scroll>
   )
 }
 
